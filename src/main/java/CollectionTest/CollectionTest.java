411a7f08b5178c4d38d13a6fb4d11a7bb81ab6e1
koheinthurako <captainthura99@gmail.com>
2024-01-09 23:20:10 +0630

    takes notes

--- src/main/java/CollectionTest/CollectionTest.java
+++ src/main/java/CollectionTest/CollectionTest.java
@@ -11,11 +11,14 @@ public class CollectionTest {
 		Test t2 = new Test("Thet", 26, "Japan", "Java");
 		
 		List<Test> list = new ArrayList<Test>();
-
+//		list.add() ထဲမှာ primitive data type တွေမဟုတ်ပဲ object တစ်ခုကိုသွားထည့်ရင် မူရင်း class ထဲမှာ
+//		declare ထားတဲ့ variable တွေထဲမှာမှ toString() method မှာ return ပြန်ထားတဲ့ variable တွေကိုပဲ
+//		list ထဲလာထည့်ပေးတယ်
 		list.add(t);
 		list.add(t2);
 		System.out.println("List : " + list);
 		System.out.println("List return test object : " + list.get(0));
+		System.out.print("Object to String : ");
 		System.out.print(list.get(0).getName());
 		System.out.print(list.get(0).getAge());
 		System.out.println(list.get(0).getLiveIn());
