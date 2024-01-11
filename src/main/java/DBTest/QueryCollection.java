50526e0347e988a6cdbd4dccf2317b72985e7bea
koheinthurako <captainthura99@gmail.com>
2024-01-11 23:36:25 +0630

    note query collection

---
+++ src/main/java/DBTest/QueryCollection.java
@@ -0,0 +1,50 @@
+package DBTest;
+
+public class QueryCollection {
+	
+//	DDL => Data Definition Language
+	
+//	create database test;
+//	use test;
+//
+//	-- DDL
+//	create table subject (
+//		SubjectNumber varchar(3) primary key,
+//	    SubjectName varchar(10)
+//	);
+//
+//	create table student (
+//		StudentNumber int primary key,
+//	    StudentName varchar(30),
+//	    gender varchar(6)
+//	);
+//
+//	create table score (
+//		StudentNumber int,
+//	    SubjectNumber varchar(3),
+//	    Score int check (score >= 0),
+//	    ExaminationDate date not null,
+//	    primary key (StudentNumber, SubjectNumber),
+//	    foreign key (StudentNumber) references student(StudentNumber),
+//	    foreign key (SubjectNumber) references subject (SubjectNumber)
+//	);
+//
+//	insert into subject values ('K11', 'English I');
+//	insert into subject values ('K12', 'English II'), ('K21', 'Maths');
+//
+//	insert into student values (6724, 'Kazuki Yamamoto', 'Male'),
+//	(6725, 'Jyugo Motoyama', 'Male'), (6816, 'Mone Yamada', 'Female'), (6817, 'Chiyo Yamamoto', 'Female');
+//
+//	insert into score values (6724, 'K11', 65, '2023-10-20'), (6724, 'K21', 85, '2023-10-21'),
+//	(6725, 'K21', 60, '2023-10-20'), (6817, 'K11', 85, '2023-10-20'),
+//	(6817, 'K12', 90, '2023-10-20'), (6817, 'K21', 95, '2023-10-21');
+//
+//	delete from score where StudentNumber = 6817 and SubjectNumber = 'K21';
+//	update student set StudentName = 'Ko Hein' where StudentNumber = 6724;
+	
+	
+//	__________________________________________________________________________________________________________
+	
+	
+
+}
