afec46b89ba44078973d933137eb190a7ce6528b
koheinthurako <captainthura99@gmail.com>
2024-01-15 23:20:06 +0630

    add subject entity

---
+++ DBTest/entityTest/subjectTest.java
@@ -0,0 +1,29 @@
+package entityTest;
+
+public class subjectTest {
+
+	private String SubjectNumber;
+	private String SubjectName;
+
+	public String getSubjectNumber() {
+		return SubjectNumber;
+	}
+
+	public void setSubjectNumber(String subjectNumber) {
+		SubjectNumber = subjectNumber;
+	}
+
+	public String getSubjectName() {
+		return SubjectName;
+	}
+
+	public void setSubjectName(String subjectName) {
+		SubjectName = subjectName;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s\t%s", SubjectNumber, SubjectName);
+	}
+
+}
